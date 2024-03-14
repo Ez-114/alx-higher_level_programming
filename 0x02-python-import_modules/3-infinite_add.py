7595b372118e0ab4f570285eb4ab3805c23b4735
ez-114 <morganezz226@gmail.com>
2024-03-14 21:46:31 +0200

    add: task 03: infinite addition:

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,11 @@
 
 # Prevent execution when imported
 if __name__ == "__main__":
-    import sys # To use argv
+    import sys  # To use argv
 
-    
+    total = 0
+    for arg in sys.argv:
+        if arg[:2] == "./":
+            continue
+        total += int(arg)
+    print(total)
