f2cb8c8ce59cc1674056e47a61eb68a528eea2b7
ez-114 <morganezz226@gmail.com>
2024-03-14 22:15:18 +0200

    Modify: task 04: trying new method

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,5 @@ if __name__ == "__main__":
     
     names = dir(hidden_4)
     for name in names:
-        if name[0] != "_":
-            print(name, end="")
-    print()
+        if name[:2] != "__":
+            print(name)
