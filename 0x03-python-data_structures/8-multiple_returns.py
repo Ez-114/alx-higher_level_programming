5ca43fe792e2cef222ae8d3ddfcdb48ed0d8426b
ez-114 <morganezz226@gmail.com>
2024-03-20 01:19:37 +0200

    Add: task 08: multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    tup = ()
+    if sentence is None:
+        tup = (0, None)
+        return (tup)
+
+    tup = (len(sentence), sentence[0])
+    return (tup)
