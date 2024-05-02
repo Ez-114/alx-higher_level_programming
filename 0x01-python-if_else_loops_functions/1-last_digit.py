09bafa9cd36424f207e81a3c2e0ae3f344ed2cb5
ez-114 <morganezz226@gmail.com>
2024-05-02 16:52:52 +0300

    fix spelling in 1-last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 mod = number % 10 if number > 10 else number % -10
-print("Last digit of {:d} is {:d}".format(number, mod), end=' ')
+print("Last digit of {:d} is {:d} and is ".format(number, mod), end='')
 if mod > 5:
-    print("and is grater than 5")
+    print("greater than 5")
 elif mod == 0:
-    print("and is 0")
+    print("0")
 elif mod < 6 and mod != 0:
-    print("and less than 6 and not 0")
+    print("less than 6 and not 0")
