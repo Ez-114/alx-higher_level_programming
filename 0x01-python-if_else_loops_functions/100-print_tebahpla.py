909ec12a1fcda574b597f484e44d381d7f95c4f3
ez-114 <morganezz226@gmail.com>
2024-05-04 19:43:14 +0300

    add task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    # Calculate the ASCII code for the lowercase and uppercase letters
-    lowercase_char = chr(ord('z') - i)
-    uppercase_char = chr(ord('Z') - i)
-
-    # Use ternary operator to choose between lowercase
-    # and uppercase based on the index `i`
-    char = lowercase_char if i % 2 == 0 else uppercase_char
-
-    # Print the chosen character without a newline at the end
-    print(char, end='')
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{}".format(chr(i - 32)), end='')
+    else:
+        print("{}".format(chr(i)), end='')
