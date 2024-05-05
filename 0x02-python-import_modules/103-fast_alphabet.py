9f91fa60b95c7eee2fa119f9b3ecb3dcf5fe50c4
ez-114 <morganezz226@gmail.com>
2024-05-05 13:17:41 +0300

    add: task 9 <0x02-python-import_modules>

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import fast_alphabet as fa
+fa.print_ALPHA()
---
+++ 0x02-python-import_modules/fast_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_ALPHA():
+    for i in range(65, 91):
+        print(chr(i), end='' if i != 90 else '\n')
