e77a67efc251d01630f58995976fdc393dc40be6
ez-114 <morganezz226@gmail.com>
2024-05-08 23:56:40 +0300

    add: task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#define Py_LIMITED_API 0x03040000
+
+#include <Python.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints basic info about python lists
+ * @p: A PyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	size_t size = PySIZE(p);
+	size_t alloc =((PyListObject *)p)->allocated;
+	size_t i;
+	PyObject *ob;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		ob = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+
+	return (0);
+}
