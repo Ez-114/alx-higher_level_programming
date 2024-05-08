a33ba2cc3c0f4db26c2e0aa9f35e29b485d06139
ez-114 <morganezz226@gmail.com>
2024-05-08 17:37:46 +0300

    mod: changed 2-replace_in_list.py to change values in the passed list insted of making a new copy

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -15,9 +15,8 @@ def replace_in_list(my_list, idx, element):
         Modified copy of OG list
     """
 
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
-    mod_list = my_list[:]
-    mod_list[idx] = element
-    return mod_list
+    my_list[idx] = element
+    return my_list
