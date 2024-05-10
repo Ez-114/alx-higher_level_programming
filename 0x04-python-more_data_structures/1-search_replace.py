76bfcee5c92fb4a3f4c19c752c92aeef6bd172bf
ez-114 <morganezz226@gmail.com>
2024-05-10 08:46:27 +0300

    add: task 1 <1-search_replace.py>

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """search_replace
+
+    replaces all occurrences of an element by another in a new list.
+
+    Args:
+        my_list: passed list
+        search: element to replace
+        replace: the new element
+    Return:
+        a list with the replaced element
+    """
+    return list(map(lambda x: replace if x == search else x ,my_list))
