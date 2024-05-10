bf52a96b5804d682a2801da772e807a6efe37610
ez-114 <morganezz226@gmail.com>
2024-05-10 09:04:01 +0300

    add: task 3 <3-common_elements.py>

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """common_elements
+    returns a set of common elements in two sets
+
+    Args:
+        set_1: set1
+        set_2: set2
+    Reutrn:
+        common elements in set 1 and 2
+    """
+    return set_1 & set_2
