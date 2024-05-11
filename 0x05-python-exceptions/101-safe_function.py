588da1787eb27637368b3574fafe37182e5e36e9
ez-114 <morganezz226@gmail.com>
2024-05-12 00:14:31 +0300

    add: task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = None
+
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
