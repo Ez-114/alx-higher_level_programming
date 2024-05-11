744d4f09a04e45f70da505dd3e4bebf5ee5a96d9
ez-114 <morganezz226@gmail.com>
2024-05-11 23:50:33 +0300

    modify: 100-safe_print_integer_err.py -> handled TypeError exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as valerr:
         sys.stderr.write("Exception: {}\n".format(valerr))
+        return False
+    except TypeError as typerr:
+        sys.stderr.write("Exception: {}\n".format(typerr))
         return False
     else:
         return True
