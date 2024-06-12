3d1d1beef58f93930c60bc1537a9d2b71fc53f28
ez-114 <morganezz226@gmail.com>
2024-06-12 17:50:39 +0300

    handel passing a negative value in a tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -117,6 +117,8 @@ class Square:
             print()
             return
 
-        for row in range(self.__size):
-            print(' ' * self.__position[0], end='')
-            print('#' * self.__size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
