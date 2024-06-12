ba7505ad9122f76ded7b61bb4d4d23882ea0b817
ez-114 <morganezz226@gmail.com>
2024-06-12 14:40:05 +0300

    fixed pycodestyle errors in '0-square' module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,17 +3,20 @@
 """
 This module defines a Square class.
 
-The Square class is currently empty and serves as a placeholder for future development.
-It can be used to represent a square in geometric calculations, graphical representations,
-or any other application where square properties and behaviors are needed.
+The Square class is currently empty and serves as
+a placeholder for future development.
+It can be used to represent a square in geometric calculations,
+graphical representations, or any other application where square properties
+and behaviors are needed.
 """
+
 
 class Square:
     """
     A class used to represent a square.
 
     This class currently does not have any attributes or methods.
-    It serves as a placeholder for future development where properties 
+    It serves as a placeholder for future development where properties
     and behaviors of a square can be added.
     """
     pass
