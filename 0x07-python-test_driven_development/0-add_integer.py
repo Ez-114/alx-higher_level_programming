8b7252a18d746378e0b004b8133035039727ac23
ez-114 <morganezz226@gmail.com>
2024-06-17 21:23:49 +0300

    test driven dev

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function called `add_integer`.
+
+The function takes 2 numbers and return their addition.
+The first number is mandatory to be passed, while the other is optional
+as it is initialized to 98.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers and return their result as an integer value.
+
+    Attributes:
+        a (int or float): The first number. Must be integer or float.
+        b (int or float, optional): The second number. Must be integer
+            or float. Defaulted to 98.
+
+    Returns:
+        int: The sum of `a` and `b` after casting to integers.
+
+    Raises:
+        TypeError: if either `a` or `b` are not integers nor floats.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Safe to return the result
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/0-add_integer.txt", optionflags=doctest.ELLIPSIS)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+# 0x07-python-test_driven_development    
+
+This project directory talks about how to use Test Driven Development (TDD)
