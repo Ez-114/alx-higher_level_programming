c69267b71fa56114dc794989db3e7b301494d342
ez-114 <morganezz226@gmail.com>
2024-06-17 21:40:54 +0300

    handled float overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -29,9 +29,9 @@ def add_integer(a, b=98):
     if b != b:
         b = 89
 
-    if type(a) not in (int, float):
+    if a is None or  (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     result = a + b
