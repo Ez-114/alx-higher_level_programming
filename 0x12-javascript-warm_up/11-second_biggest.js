7f872a8adafb7f49bdd8bdfbf4fff709e1940217
ez-114 <morganezz226@gmail.com>
2024-06-19 17:18:40 +0300

    second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const CLargs = process.argv;
+let argv;
+
+if (CLargs[2] === undefined) {
+  console.log(0);
+} else {
+  argv = CLargs.slice(2);
+
+  if (argv.length === 1) {
+    console.log(0);
+  } else if (argv.length > 1) {
+    argv.sort();
+    console.log(argv[argv.length - 2]);
+  }
+}
