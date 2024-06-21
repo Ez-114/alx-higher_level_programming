0772b2f1ae0f0fd99afd1b8ee69ee2ef571557ef
ez-114 <morganezz226@gmail.com>
2024-06-22 01:52:49 +0300

    added new test for  module

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,21 +24,13 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if either `a` or `b` are not integers nor floats.
     """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
-
-    if a is None or  (type(a) is not int and type(a) is not float):
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
     result = a + b
     if result == float('inf') or result == -float('inf'):
         return 89
-
-    # Safe to return the result
     return int(a) + int(b)
 
 
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""
+This module defines the ``matrix_divided`` function.
+
+This function takes 2 inputs and return 1. The first input is a 2D matrix,
+list of lists, and the second input is the number that will divide every
+element in the matrix.
+
+First imput must be a list of lists of integers or floats or both.
+The second input must be an integer or a float.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides matrix elements by the number ``div``.
+
+    Args:
+        matrix (list): First input. Must be list of lists.
+            Must contain integers or floats.
+        div (int or float): Second input. Must be an integer or float.
+            Must not be 0.
+
+    Raises:
+        TypeError: if matrix is not a list of lists or
+            if rows in matrix have different lengths or
+            div or matrix elements are not integers or floats.
+        ZeroDivisionError: if div is Zero 0.
+    """
+
+    # first check for div
+    if div is None or not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Now check for matrix and its elements
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) \
+                of integers/floats")
+
+    og_len = len(matrix[0])  # This contains the length of first row
+    divided_matrix = []  # to store the divided mat
+    for i, row in enumerate(matrix):
+        if og_len != len(row):  # Compares each row with the first row's len
+            raise TypeError("Each row of the matrix must have the same size")
+
+        divided_row = []  # to store each divided row
+        # Iterate on each element
+        for j, col in enumerate(row):
+            # handel elements types
+            if not isinstance(col, (float, int)):
+                raise TypeError("matrix must be a matrix (list of lists) \
+                        of integers/floats")
+
+            # do calculation
+            new_val = col / div
+            divided_row.append(round(new_val, 2))
+
+        divided_matrix.append(divided_row)
+
+    return divided_matrix
