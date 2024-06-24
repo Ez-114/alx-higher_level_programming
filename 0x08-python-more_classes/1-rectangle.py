027d9a2aa0bd76625d1340d976cf366b35a13ac1
ez-114 <morganezz226@gmail.com>
2024-06-24 19:39:58 +0300

    corrected errors

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -56,7 +56,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
 
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
 
         self.__width = value
 
@@ -84,6 +84,6 @@ class Rectangle:
             raise TypeError("height must be an integer")
 
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
 
         self.__height = value
