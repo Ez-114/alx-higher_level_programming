d881b22bc43049e110711bdcd6ab65d132151979
ez-114 <morganezz226@gmail.com>
2024-06-27 20:38:17 +0300

    fix

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
 """
-This module defines a Geometry class.
+This module defines a BaseGeometry class.
 
-The Geometry class is currently empty and serves as
+The BaseGeometry class is currently empty and serves as
 a placeholder for future development.
-It can be used to represent a Geometry in geometric calculations,
-graphical representations, or any other application where Geometry properties
+It can be used to represent a BaseGeometry in geometric calculations,
+graphical representations, or any other application where BaseGeometry properties
 and behaviors are needed.
 """
 
 
-class Geometry:
+class BaseGeometry:
     """
-    A class used to represent a Geometry.
+    A class used to represent a BaseGeometry.
 
     This class currently does not have any attributes or methods.
     It serves as a placeholder for future development where properties
-    and behaviors of a Geometry can be added.
+    and behaviors of a BaseGeometry can be added.
     """
     pass
