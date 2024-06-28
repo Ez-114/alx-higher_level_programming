1cc457c48c5b5fc1988226326f16e6d643df62d7
ez-114 <morganezz226@gmail.com>
2024-06-28 04:38:03 +0300

    Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a BaseGeometry class.
+
+The BaseGeometry class is currently empty and serves as
+a placeholder for future development.
+It can be used to represent a BaseGeometry in geometric calculations,
+graphical representations, or any other application where BaseGeometry
+properties and behaviors are needed.
+"""
+
+
+class BaseGeometry:
+    """
+    A class used to represent a BaseGeometry.
+
+    This class currently does not have any attributes or methods.
+    It serves as a placeholder for future development where properties
+    and behaviors of a BaseGeometry can be added.
+    """
+
+    def area(self):
+        """
+        Raises an Exception to indicate that the area method is not implemented.
+
+        Raises:
+            Exception: Always raises an exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
