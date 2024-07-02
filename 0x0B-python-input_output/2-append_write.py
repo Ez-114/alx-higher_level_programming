76e78b2b3ceadc216a6ec326d77922cd0b8ea664
ez-114 <morganezz226@gmail.com>
2024-07-02 17:18:52 +0300

    appending text

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/user/bin/python3
+"""2-append_write
+This module defines the `append_write` function that writes to a file
+without deleting its old content.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends text to a file.
+
+    Args:
+        filename (str, optional): the file name along with its path.
+                Defaults to an empty string.
+        text (str, optional): the text to insert. Defaults to an empty string.
+    Returns:
+        (int): total number of written bytes.
+    """
+    total_written = 0
+    with open(filename, mode="a", encoding="utf-8") as f:
+        total_written = f.write(text)
+    return total_written
---
+++ 0x0B-python-input_output/2-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+append_write = __import__("2-append_write").append_write
+
+print(append_write("tex_file.txt", "the new added text\n"))
