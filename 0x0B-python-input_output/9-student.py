02482f6ad192840a944dc6722860b1ef28b2635e
ez-114 <morganezz226@gmail.com>
2024-07-04 19:06:10 +0300

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""9-student.py"""
+import json
+
+
+class Student:
+    """Student calss"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to_json method"""
+        return self.__dict__
