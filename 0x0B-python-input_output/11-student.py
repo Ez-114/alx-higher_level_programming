2a0c543f7c04d9d435fc6f27c3bebd906e27106d
ez-114 <morganezz226@gmail.com>
2024-07-04 19:49:37 +0300

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""10-student.py"""
+
+
+class Student:
+    """Student calss"""
+
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json method"""
+        if attrs is None:
+            return self.__dict__
+
+        if isinstance(attrs, list) \
+                and all(isinstance(attr, str) for attr in attrs):
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload"""
+        for key, val in json.items():
+            setattr(self, key, val)
