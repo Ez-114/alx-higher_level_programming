cdcfa7c4db2e0fe09c4b4f450d4775d87068cde9
ez-114 <morganezz226@gmail.com>
2024-07-07 16:33:21 +0300

    Now I can update attributes in a single methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -182,3 +182,19 @@ class Rectangle(Base):
                                                     self.width,
                                                     self.height
                                                 )
+
+    def update(self, *args):
+        """
+        Updates the attributes of the instance.
+        The values should be passed in this order:
+            1- id
+            2- width
+            3- height
+            4- x
+            5- y
+        """
+        self.id = args[0] if len(args) > 0 else self.id
+        self.width = args[1] if len(args) > 1 else self.width
+        self.height = args[2] if len(args) > 2 else self.height
+        self.x = args[3] if len(args) > 3 else self.x
+        self.y = args[4] if len(args) > 4 else self.y
