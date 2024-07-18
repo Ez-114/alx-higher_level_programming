931ccec9b270823688094c4f86ba06926faef56c
ez-114 <morganezz226@gmail.com>
2024-07-18 19:17:56 +0300

    added a csv reader

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,7 @@ the base of all other classes in this project.
 """
 import json
 import os
+import csv
 
 
 class Base:
@@ -117,6 +118,65 @@ class Base:
             return []
         with open(filename, 'r') as file:
             return [
-                    cls.create(**d)
-                    for d in Base.from_json_string(file.read())
-                ]
+                cls.create(**d)
+                for d in Base.from_json_string(file.read())
+            ]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Writes the CSV string representation of list_objs to a CSV file.
+
+        Args:
+            list_objs (object): Is a list of instances who inherits of Base.
+
+        Example:
+            list of Rectangle or list of Square instances.
+        """
+
+        filename = f"{cls.__name__}.csv"
+        with open(filename, mode='w', encoding='utf-8', newline='')\
+                as csvfile:
+            writer = csv.writer(csvfile)
+            if cls.__name__ == "Rectangle":
+                writer.writerow(['id', 'width', 'height', 'x', 'y'])
+
+                for obj in list_objs:
+                    writer.writerow(
+                        [obj.id, obj.width, obj.height, obj.x, obj.y]
+                    )
+            elif cls.__name__ == "Square":
+                writer.writerow(['id', 'size', 'x', 'y'])
+
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Returns a list of instances.
+
+        The filename must be: <Class name>.csv
+            - example: Rectangle.csv
+        If the file doesn't exist, return an empty list.
+        Otherwise, return a list of instances
+
+        - the type of these instances depends on cls.
+
+        Returns:
+            list: List of instances.
+        """
+
+        filename = f"{cls.__name__}.csv"
+        if not os.path.exists(filename):
+            return []
+
+        with open(filename, mode='r', encoding='utf-8', newline='')\
+                as csvfile:
+            reader = csv.DictReader(csvfile)
+            list_objs = []
+            for row in reader:
+                row = {key: int(value) for key, value in row.items()}
+                list_objs.append(cls.create(**row))
+
+        return list_objs
