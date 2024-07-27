14135e2c34cecc8e535f6a9bf83dac591a0d4ae8
ez-114 <morganezz226@gmail.com>
2024-07-27 19:55:17 +0300

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Change the character set and collation of the database
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the table
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the 'name' field in 'first_table'
+ALTER TABLE first_table
+    MODIFY name VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
