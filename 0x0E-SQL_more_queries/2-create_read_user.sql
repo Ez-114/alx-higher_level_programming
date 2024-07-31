fcd3d7a9ab6869883a2a41c996cd7754f8344f4f
Ez-114 <morganezz226@gmail.com>
2024-07-31 14:42:37 +0300

    this will work this time for sure

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,13 +2,13 @@
 -- and granting user "user_0d_2" his required privilleges
 
 -- Creating "htbn_0d_2" database on local machine
-CREATE DATABASE IF NOT EXISTS 'htbn_0d_2';
+CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 
 -- Create "user_0d_2" user on "localhost"
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant the created user the SELECT privellege only
-GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `htbn_0d_2`.* TO 'user_0d_2'@'localhost';
 
 -- Apply changes
 FLUSH PRIVILEGES;
