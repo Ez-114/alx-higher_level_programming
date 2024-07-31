6d7d34d5309dff1c7a858fcecae5c955d692b750
Ez-114 <morganezz226@gmail.com>
2024-07-31 14:33:31 +0300

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Creates user "user_0d_2"@"localhost" and database "htbn_0d_2"
+-- and granting user "user_0d_2" his required privilleges
+
+-- Creating "htbn_0d_2" database on local machine
+CREATE DATABASE IF NOT EXISTS 'htbn_0d_2';
+
+-- Create "user_0d_2" user on "localhost"
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant the created user the SELECT privellege only
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
