3681b6601b28fd1a2c1e4cf69866489f2178a2ff
Ez-114 <morganezz226@gmail.com>
2024-08-01 18:50:29 +0300

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,13 +1,14 @@
 -- list all genres not in the 'dexter' show
 SELECT g.name
 	FROM tv_genres AS g
-	WHERE g.name NOT IN 
-		(SELECT g.name
-			FROM tv_genres AS g
-				JOIN tv_show_genres AS sg
+	WHERE g.name NOT IN (
+    		SELECT g.name
+    			FROM tv_genres AS g
+    				JOIN tv_show_genres AS sg
 				ON g.id = sg.genre_id
-
+    				
 				JOIN tv_shows AS s
 				ON sg.show_id = s.id
-			WHERE s.title = 'Dexter'
-			ORDER BY g.name ASC);
+    			WHERE s.title = 'Dexter'
+		) ORDER BY g.name ASC;
+
